e2532d87c772094c7333e3a4c7a9a3407431dceb
ajs11 <ajs11@illinois.edu>
2019-04-22 18:40:34 -0600

    Spinner has values in dropdown menu. Text box changes with selection from spinner.

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -7,28 +7,30 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final TextView text = findViewById(R.id.textView);
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-    }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String station = parent.getItemAtPosition(position).toString();
+                text.setText(station);
+            }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
     }
 }

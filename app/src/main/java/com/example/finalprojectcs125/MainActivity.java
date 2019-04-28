48374b8114d56878f584a068a914c69267f83bd2
ajs11 <ajs11@illinois.edu>
2019-04-27 19:29:49 -0600

    Started work on API.

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.finalprojectcs125;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+
+    private String jsonString = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView text = findViewById(R.id.textView);
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -25,11 +41,31 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String API_KEY = "d37555ccc09141848543ab21e287b560";
+                String currentStation = "";
                 String station = parent.getItemAtPosition(position).toString();
-                // make API request
+                if (station.equals("Clark/Lake")) {
+                    currentStation = "40380";
+                }
+                if (station.equals("Belmont")) {
+                    currentStation="41320";
+                }
+                String url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + API_KEY + "&mapid=" + currentStation + "&outputType=JSON&max=3";
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        jsonString = response;
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        text.setText("oof");
+                    }
+                });
+                requestQueue.add(stringRequest);
                 // get info from API request
                 // display the data from API request
-                text.setText(station);
+                text.setText(jsonString);
             }
 
             @Override

2b6b9fae439756233663b77249a3bd54c1d1fc37
ajs11 <ajs11@illinois.edu>
2019-04-28 14:10:22 -0600

    Parsing.

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -16,31 +16,18 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextViewResult;
-
     private RequestQueue requestQueue;
-
-    private String jsonString = null;
-
+    private String jsonString;
     private String url;
-
     private String arrivalTime;
     private String towards;
     private String lineColor;
-
-    private String shortString;
-
+    private String shortString = "";
+    private boolean causedError = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,17 +44,14 @@ public class MainActivity extends AppCompatActivity {
                 String API_KEY = "d37555ccc09141848543ab21e287b560";
                 String currentStation = "";
                 String station = parent.getItemAtPosition(position).toString();
-                if (station.equals("Clark/Lake")) {
-                    currentStation = "40380";
-                }
                 if (station.equals("Belmont")) {
                     currentStation="41320";
                 }
-                url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + API_KEY + "&mapid=" + currentStation + "&outputType=JSON&max=6";
-                // get info from API request
-                // display the data from API request
+                if (station.equals("Clark/Lake")) {
+                    currentStation = "40380";
+                }
+                url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + API_KEY + "&mapid=" + currentStation + "&outputType=JSON&max=3";
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) { }
         });
@@ -78,69 +62,59 @@ public class MainActivity extends AppCompatActivity {
     }
     public void process(RequestQueue requestQueue, String url) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-
             @Override
             public void onResponse(String response){
-                    jsonString = response;
-
-                JsonParser parser = new JsonParser();
-                JsonObject result = parser.parse(response).getAsJsonObject();
-                JsonObject ctatt = result.get("ctatt").getAsJsonObject();
-                JsonArray eta = ctatt.get("eta").getAsJsonArray();
-
-                for (int i = 0; i < eta.size(); i++) {
-                    JsonObject etaArray = eta.get(i).getAsJsonObject();
-                    arrivalTime = etaArray.getAsJsonPrimitive("arrT").
-                            getAsString();
-                    towards = etaArray.getAsJsonPrimitive("destNm").getAsString();
-                    lineColor = etaArray.getAsJsonPrimitive("rt").getAsString();
-                    if (lineColor.equals("G")) {
-                        lineColor = "Green Line";
-                    }
-                    if (lineColor.equals("Red")) {
-                        lineColor = "Red Line";
-                    }
-                    if (lineColor.equals("Blue")) {
-                        lineColor = "Blue Line";
-                    }
-                    if (lineColor.equals("Brn")) {
-                        lineColor = "Brown Line";
-                    }
-                    if (lineColor.equals("Org")) {
-                        lineColor = "Orange Line";
-                    }
-                    if (lineColor.equals("P")) {
-                        lineColor = "Purple line";
-                    }
-                    if (lineColor.equals("Pink")) {
-                        lineColor = "Pink Line";
-                    }
-                    if (lineColor.equals("Y")) {
-                        lineColor = "Yellow Line";
-                    }
-
-                    shortString = "Headed To: " + towards + "\n Route: " + lineColor +
-                            "\n Arrival Time: " + arrivalTime.substring(11, 19);
-
-
-                }
-
-
-
-
-
-
-
+                jsonString = response;
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 jsonString = error.getMessage();
+                causedError = true;
             }
         });
-
         requestQueue.add(stringRequest);
-        //parse jsonString`
-        //get arrival time
+        if (causedError) {
+            shortString = jsonString;
+            causedError = false;
+            return;
+        }
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(jsonString).getAsJsonObject();
+        JsonObject ctatt = result.get("ctatt").getAsJsonObject();
+        JsonArray eta = ctatt.get("eta").getAsJsonArray();
+        for (int i = 0; i < eta.size(); i++) {
+            JsonObject etaArray = eta.get(i).getAsJsonObject();
+            arrivalTime = etaArray.getAsJsonPrimitive("arrT").
+                    getAsString();
+            towards = etaArray.getAsJsonPrimitive("destNm").getAsString();
+            lineColor = etaArray.getAsJsonPrimitive("rt").getAsString();
+            if (lineColor.equals("G")) {
+                lineColor = "Green Line";
+            }
+            if (lineColor.equals("Red")) {
+                lineColor = "Red Line";
+            }
+            if (lineColor.equals("Blue")) {
+                lineColor = "Blue Line";
+            }
+            if (lineColor.equals("Brn")) {
+                lineColor = "Brown Line";
+            }
+            if (lineColor.equals("Org")) {
+                lineColor = "Orange Line";
+            }
+            if (lineColor.equals("P")) {
+                lineColor = "Purple line";
+            }
+            if (lineColor.equals("Pink")) {
+                lineColor = "Pink Line";
+            }
+            if (lineColor.equals("Y")) {
+                lineColor = "Yellow Line";
+            }
+            shortString = shortString + "Headed To: " + towards + "\n Route: " + lineColor +
+                    "\n Arrival Time: " + arrivalTime.substring(11, 19) + "\n";
+        }
     }
 }
